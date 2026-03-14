bced9fa4cd1c77d124469b531cbaf9c535e2c193
Pavel Ostroukhov <pavel.ostroukhov@swift-games.com>
2026-03-14 13:14:01 +0100

    Update info

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   // blue, cyan, gray, green, orange, pink, purple, red, teal, yellow
 
   // edit this variable to change the color theme
-  const color = "teal";
+  const color = "orange";
 
   return (
     <>
@@ -23,7 +23,6 @@ function App() {
       <Experience color={color} />
       <Projects color={color} />
       <Contact color={color} />
-      <Footer />
     </>
   );
 }
