173c9436ad4363d05c762dd5c21fed936420307e
Pavel Ostroukhov <pavel.ostroukhov@swift-games.com>
2026-03-14 13:43:59 +0100

    Fix error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
 
 function App() {
   // Available Colours:
